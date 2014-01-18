c15f9aff9d8f38af4e7d65dc10636e1ef2005ee5
Dillon <melanclock@darkstar.example.net>
2014-01-18 15:38:45 -0800

    Parser for the import function version 1

---
+++ importParser.py
@@ -0,0 +1,28 @@
+#input format The import/export format standard format will be a TAB separated list of entry fields. 
+#tab seperated entry\n
+#tab seperated entry\n
+#...
+
+#given an input line, determine if it is valid
+def isValidInput(inputString):
+    #DEFINITELY not good enough. It will work for now though.
+    if inputSring:
+        return true
+    else:
+        return false
+
+#given a file will output a list of maps
+def importParse(importFile):
+    _FORMAT = ["fname","lname","addr","city","state","zip","phone","email"]
+    entryList = list()
+    for line in importFile:
+        #split the line on tab characters
+        #turn it into a map with the format list as the keys
+        #add it to the list
+        if isValidInput(line):
+            entryList.append(dict(zip(_FORMAT, line.split('\t'))))
+        else:
+            print "'", line, "' is invalid."
+    if !entryList:
+        print "Empty input"
+    return entryList
