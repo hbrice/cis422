e1da4d75b5858eef48080adb24f72fb9969a3e18
Lucas Rondenet <lrondenet@gmail.com>
2014-01-20 15:25:37 -0800

    added __eq__ and changed var names in contacts.py

--- cis422project1/model/contact.py
+++ cis422project1/model/contact.py
@@ -6,40 +6,39 @@ class contact:
     def __init__(self, fname, lname):
         self.fname = fname
         self.lname = lname
-        self.addresses = []
-        self.emails = []
-        self.phoneNumbers = []
+        self.addressList = []
+        self.emailList = []
+        self.phoneNumberList = []
 
     def __eq__(self, other):
         return (self.fname == other.fname) and (self.lname == other.lname) \
-            and (self.addresses == other.addresses) and (self.emails == other.emails) \
-            and (self.phoneNumbers)
-
+            and (self.addressList == other.addressList) and (self.emailList == other.emailList) \
+            and (self.phoneNumberList == other.phoneNumberList)
 
     def addAddresses(self, addr):
-        self.addresses.append(addr)
+        self.addressList.append(addr)
 
     def addEmail(self, email):
-        self.emails.append(email)
+        self.emailList.append(email)
 
     def addPhoneNumber(self, phoneNumber):
-        self.phoneNumbers.append(phoneNumber)
+        self.phoneNumberList.append(phoneNumber)
 
     def removeAddress(self, addr):
         try:
-            self.addresses.remove(addr)
+            self.addressList.remove(addr)
         except ValueError:
             return -1
 
     def removeEmail(self, email):
         try:
-            self.emails.remove(email)
+            self.emailList.remove(email)
         except ValueError:
             return -1
 
     def removePhoneNumber(self, phoneNumber):
         try:
-            self.phoneNumbers.remove(phoneNumber)
+            self.phoneNumberList.remove(phoneNumber)
         except ValueError:
             return -1
 
